d35ed3259bc990b9d9416728e01be94ddfb15ae1
Bruno Amorim <brunoamorim39@yahoo.com>
2022-02-28 18:53:17 -0500

    Preliminary fan speed reading

--- application/controller_master.py
+++ application/controller_master.py
@@ -49,7 +49,7 @@ FAN_MID = 50
 FAN_HIGH = 75
 FAN_MAX = 100
 
-STALL_TIME = 100                  # [sec] Controller sleep time
+STALL_TIME = 50                  # [sec] Controller sleep time
 
 
 
@@ -57,7 +57,7 @@ def set_fan_speed(target_fan, fan_speed):
     target_fan.start(fan_speed)
     return
 
-def condition_analysis():
+def fan_control():
     # Temperature Control
     current_temperature = DataLog.query.order_by(DataLog.id.desc()).first().temperature
 
@@ -85,24 +85,46 @@ def condition_analysis():
         set_fan_speed(humidifer_fan, FAN_MID)
         print(f'CARBON DIOXIDE READING OF {current_carbon_dioxide} - SETTING HUMIDIFER FAN SPEED TO {FAN_MID}%')
 
+def read_fan_speed(fan, pulse):
+    dt = time.time() - initial_time
+    if dt < 0.005:
+        return
+
+    frequency = 1 / dt
+    rpm = (frequency / pulse) * 60
+    print(f'{fan} fan speed = {rpm} RPM')
+
 try:
     print('Initializing control...')
     # Initialize PWM fan control
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
+
     GPIO.setup(HUMIDIFIER_FAN_PWM_PIN, GPIO.OUT, initial=GPIO.LOW)
     # GPIO.setup(INTAKE_FAN_PWM_PIN, GPIO.OUT, initial=GPIO.LOW)
     # GPIO.setup(EXHAUST_FAN_PWM_PIN, GPIO.OUT, initial=GPIO.LOW)
+
+    GPIO.setup(HUMIDIFIER_FAN_RPM_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    # GPIO.setup(INTAKE_FAN_RPM_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    # GPIO.setup(EXHAUST_FAN_RPM_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
     humidifer_fan = GPIO.PWM(HUMIDIFIER_FAN_PWM_PIN, HUMIDIFIER_FAN_PWM_FREQ)
     # intake_fan = GPIO.PWM(INTAKE_FAN_PWM_PIN, INTAKE_FAN_PWM_FREQ)
     # exhaust_fan = GPIO.PWM(EXHAUST_FAN_PWM_PIN, EXHAUST_FAN_PWM_FREQ)
+
     set_fan_speed(humidifer_fan, FAN_OFF)
     # set_fan_speed(intake_fan, FAN_OFF)
     # set_fan_speed(exhaust_fan, FAN_OFF)
 
-    # Realtime fan control
+    # Realtime control
     while True:
-        condition_analysis()
+        fan_control()
+
+        initial_time = time.time()
+        GPIO.add_event_detect(HUMIDIFIER_FAN_RPM_PIN, GPIO.FALLING, read_fan_speed('Humidifier', HUMIDIFER_FAN_RPM_PULSE))
+        GPIO.add_event_detect(INTAKE_FAN_RPM_PIN, GPIO.FALLING, read_fan_speed('Intake', INTAKE_FAN_RPM_PULSE))
+        GPIO.add_event_detect(EXHAUST_FAN_RPM_PIN, GPIO.FALLING, read_fan_speed('Exhaust', EXHAUST_FAN_RPM_PULSE))
+
         time.sleep(STALL_TIME)
 
 except KeyboardInterrupt:
@@ -115,3 +137,4 @@ except KeyboardInterrupt:
     set_fan_speed(humidifer_fan, FAN_HIGH)
     # set_fan_speed(intake_fan, FAN_HIGH)
     # set_fan_speed(exhaust_fan, FAN_HIGH)
+    GPIO.cleanup()
