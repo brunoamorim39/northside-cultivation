4cb944a749fda6f70c5403312880727dea81a42e
Bruno Amorim <brunoamorim39@yahoo.com>
2022-02-28 19:02:36 -0500

    bug fix

--- application/controller_master.py
+++ application/controller_master.py
@@ -116,24 +116,24 @@ try:
     # set_fan_speed(intake_fan, FAN_OFF)
     # set_fan_speed(exhaust_fan, FAN_OFF)
 
+    # Fan RPM data collection
+    initial_time = time.time()
+    GPIO.add_event_detect(HUMIDIFIER_FAN_RPM_PIN, GPIO.FALLING)
+    # GPIO.add_event_detect(INTAKE_FAN_RPM_PIN, GPIO.FALLING)
+    # GPIO.add_event_detect(EXHAUST_FAN_RPM_PIN, GPIO.FALLING)
+
+    if GPIO.event_detected(HUMIDIFIER_FAN_RPM_PIN):
+        read_fan_speed('Humidifier', HUMIDIFER_FAN_RPM_PULSE)
+    
+    if GPIO.event_detected(INTAKE_FAN_RPM_PIN):
+        read_fan_speed('Intake', INTAKE_FAN_RPM_PULSE)
+
+    if GPIO.event_detected(EXHAUST_FAN_RPM_PIN):
+        read_fan_speed('Exhaust', EXHAUST_FAN_RPM_PULSE)
+
     # Realtime control
     while True:
         fan_control()
-
-        initial_time = time.time()
-        GPIO.add_event_detect(HUMIDIFIER_FAN_RPM_PIN, GPIO.FALLING)
-        # GPIO.add_event_detect(INTAKE_FAN_RPM_PIN, GPIO.FALLING)
-        # GPIO.add_event_detect(EXHAUST_FAN_RPM_PIN, GPIO.FALLING)
-
-        if GPIO.event_detected(HUMIDIFIER_FAN_RPM_PIN):
-            read_fan_speed('Humidifier', HUMIDIFER_FAN_RPM_PULSE)
-        
-        if GPIO.event_detected(INTAKE_FAN_RPM_PIN):
-            read_fan_speed('Intake', INTAKE_FAN_RPM_PULSE)
-
-        if GPIO.event_detected(EXHAUST_FAN_RPM_PIN):
-            read_fan_speed('Exhaust', EXHAUST_FAN_RPM_PULSE)
-
         time.sleep(STALL_TIME)
 
 except KeyboardInterrupt:
