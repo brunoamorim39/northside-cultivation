a84fc2807f9a9ef86dc9ec7934d59e4de8bc10a7
Bruno Amorim <brunoamorim39@yahoo.com>
2022-03-29 18:56:44 -0400

    Added test script for remote control of the humidifier's outlet

---
+++ testing/rc_outlet.py
@@ -0,0 +1,22 @@
+import time
+import RPi.GPIO as GPIO
+
+GPIO_PIN = 10
+
+try:
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(GPIO_PIN, GPIO.OUT, initial=GPIO.LOW)
+
+    while True:
+        try:
+            print('Turning button ON...')
+            GPIO.output(GPIO_PIN, GPIO.HIGH)
+            time.sleep(30)
+            print('Turning button OFF...')
+            GPIO.output(GPIO_PIN, GPIO.LOW)
+            time.sleep(30)
+        except RuntimeError as error:
+            print(error.args[0])
+except KeyboardInterrupt:
+    GPIO.cleanup()
