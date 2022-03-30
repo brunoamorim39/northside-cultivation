288efd46bb4831a7458c1adfd8e48672a5567fcd
brunoamorim39 <brunoamorim39@yahoo.com>
2022-03-29 21:48:59 -0400

    debug rc control

--- testing/rc_outlet.py
+++ testing/rc_outlet.py
@@ -6,19 +6,19 @@ GPIO_PIN = 10
 try:
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(GPIO_PIN, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(GPIO_PIN, GPIO.OUT, initial=GPIO.HIGH)
 
     while True:
         try:
             print('Turning button ON...')
-            GPIO.output(GPIO_PIN, GPIO.HIGH)
-            time.sleep(0.5)
             GPIO.output(GPIO_PIN, GPIO.LOW)
+            time.sleep(0.5)
+            GPIO.output(GPIO_PIN, GPIO.HIGH)
             time.sleep(30)
             print('Turning button OFF...')
             GPIO.output(GPIO_PIN, GPIO.LOW)
             time.sleep(0.5)
-            GPIO.output(GPIO_PIN, GPIO.LOW)
+            GPIO.output(GPIO_PIN, GPIO.HIGH)
             time.sleep(30)
         except RuntimeError as error:
             print(error.args[0])
